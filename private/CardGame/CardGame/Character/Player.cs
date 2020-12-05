b75a58d8809b2b7247468385dfd9bdc1d8d653ab
hasu <hasu@microsoft.com>
2020-12-05 08:15:37 +0000

    initial

---
+++ private/CardGame/CardGame/Character/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardGame.Character
+{
+    class Player
+    {
+        private int healthPoint;
+        private int manaPoint;
+        private int energyPoint;
+        private int ragePoint;
+    }
+}
